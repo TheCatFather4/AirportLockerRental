6582a2a0ac2e0dc6584efb99b42f64a1af56842e
TheCatFather4 <marcelloja.csharp@gmail.com>
2025-06-29 17:24:05 -0400

    Added Unit Test Project
    Refactored Locker Manager methods to return data.

---
+++ AirportLockerRental/AirportLockerRental.UI/Actions/LockerManager.cs
@@ -0,0 +1,45 @@
+﻿using AirportLockerRental.DTOS;
+
+namespace AirportLockerRental.Actions
+{
+    public class LockerManager
+    {
+        private LockerContents[] _lockers = new LockerContents[100];
+
+        public LockerContents GetLockerContents(int number)
+        {
+            return _lockers[number - 1];
+        }
+
+        public bool IsAvailable(int number)
+        {
+            return _lockers[number - 1] == null;
+        }
+
+        public LockerContents RentLocker(int number)
+        {
+            LockerContents contents = new LockerContents();
+
+            contents.RenterName = ConsoleIO.GetRequiredString("Enter renter name: ");
+            contents.Description = ConsoleIO.GetRequiredString("Enter item description: ");
+
+            return _lockers[number - 1] = contents;
+        }
+
+        public LockerContents ReturnLocker(int number)
+        {
+            return _lockers[number - 1] = null;
+        }
+
+        public void ViewAllLockers()
+        {
+            for (int i = 0; i < _lockers.Length; i++)
+            {
+                if (_lockers[i] != null)
+                {
+                    ConsoleIO.PrintLockerContents(i + 1, _lockers[i]);
+                }
+            }
+        }
+    }
+}
---
+++ AirportLockerRental/AirportLockerRental.UI/Workflow/App.cs
@@ -0,0 +1,75 @@
+﻿using AirportLockerRental.Actions;
+using AirportLockerRental.DTOS;
+
+namespace AirportLockerRental.Workflow
+{
+    public static class App
+    {
+        public static void Run()
+        {
+            LockerManager mgr = new LockerManager();
+
+            do
+            {
+                ConsoleIO.PrintMenu();
+                int choice = ConsoleIO.GetMenuChoice();
+
+                if (choice < 4)
+                {
+                    int number = ConsoleIO.GetLockerNumber();
+
+                    if (choice == 1)
+                    {
+                        if (!mgr.IsAvailable(number))
+                        {
+                            LockerContents contents = mgr.GetLockerContents(number);
+                            ConsoleIO.PrintLockerContents(number, contents);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Locker number {number} is currently empty.");
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        if (mgr.IsAvailable(number))
+                        {
+                            LockerContents contents = mgr.RentLocker(number);
+                            Console.WriteLine($"Locker number {number} has been rented for {contents.Description} storage.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Locker number {number} is already rented.");
+                        }
+                    }
+                    else
+                    {
+                        if (!mgr.IsAvailable(number))
+                        {
+                            LockerContents c1 = mgr.GetLockerContents(number);
+                            string item = c1.Description;
+                            LockerContents c2 = mgr.ReturnLocker(number);
+                            Console.WriteLine($"Locker number {number} rental has ended. Please take your {item}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Locker number {number} is already empty.");
+                        }
+                    }
+                }
+                else if (choice == 4)
+                {
+                    mgr.ViewAllLockers();
+                }
+                else
+                {
+                    break;
+                }
+
+                ConsoleIO.AnyKey();
+                Console.Clear();
+            }
+            while (true);
+        }
+    }
+}
