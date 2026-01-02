8117ef6b46ce1384dd50bfeb1cabc294d494391e
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-01-02 15:30:29 -0500

    Deleted old files

--- AirportLockerRental/AirportLockerRental.UI/DTOS/LockerContents.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AirportLockerRental.DTOS
-{
-    public class LockerContents
-    {
-        public string RenterName { get; set; }
-        public string Description { get; set; }
-    }
-}
